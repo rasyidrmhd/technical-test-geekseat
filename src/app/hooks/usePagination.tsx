7319c8d5acc7f05c9d1beea94834a7a9cfee79ea
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-09-01 04:59:07 +0700

    tidying up layout and add pagination

---
+++ src/app/hooks/usePagination.tsx
@@ -0,0 +1,169 @@
+import {
+  Button,
+  Center,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { first, last } from "lodash";
+import React from "react";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+
+const DOTS = "...";
+
+const usePagination = ({
+  totalCount = 0,
+  currentCount = 0,
+  siblingCount = 1,
+}: {
+  totalCount: number;
+  currentCount: number;
+  siblingCount?: number;
+}) => {
+  const [pageSize, setPageSize] = React.useState<number>(10);
+  const [currentPage, setCurrentPage] = React.useState<number>(1);
+
+  const range = React.useCallback((start: number, end: number) => {
+    const length = end - start + 1;
+    return Array.from({ length }, (_, idx) => idx + start);
+  }, []);
+
+  const paginationRange = React.useMemo(() => {
+    const totalPageCount = Math.ceil(totalCount / pageSize);
+    const totalPageNumbers = siblingCount + 5;
+
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount
+    );
+
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+    const firstPageIndex = 1;
+    const lastPageIndex = totalPageCount;
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      const leftItemCount = 3 + 2 * siblingCount;
+      const leftRange = range(1, leftItemCount);
+
+      return [...leftRange, DOTS, totalPageCount];
+    }
+
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      const rightItemCount = 3 + 2 * siblingCount;
+      const rightRange = range(
+        totalPageCount - rightItemCount + 1,
+        totalPageCount
+      );
+      return [firstPageIndex, DOTS, ...rightRange];
+    }
+
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex);
+      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
+    }
+  }, [totalCount, pageSize, siblingCount, currentPage, range]);
+
+  const renderPageNumber = React.useMemo(
+    () => (
+      <Flex
+        gap="20px"
+        alignItems="center"
+        w="full"
+        justifyContent="space-between"
+      >
+        <Text>
+          Showing {currentCount} data from {totalCount} data
+        </Text>
+        <Flex gap="20px">
+          <Center
+            w="32px"
+            h="32px"
+            p="0"
+            size="sm"
+            as={Button}
+            bg="white"
+            variant="outline"
+            isDisabled={currentPage === first(paginationRange)}
+            onClick={() => setCurrentPage(currentPage - 1)}
+          >
+            <ChevronLeftIcon boxSize="18px" />
+          </Center>
+
+          <Flex gap="5px">
+            {paginationRange?.map((page, idx) => {
+              if (page === DOTS) {
+                return (
+                  <Flex alignSelf="flex-end" mx="2px" key={idx}>
+                    &#8230;
+                  </Flex>
+                );
+              }
+
+              return (
+                <Center
+                  w="32px"
+                  h="32px"
+                  as={Button}
+                  variant={page === currentPage ? "solid" : "outline"}
+                  p="0"
+                  size="sm"
+                  colorScheme={page === currentPage ? "blue" : "gray"}
+                  onClick={() => setCurrentPage(Number(page))}
+                  key={idx}
+                >
+                  {page}
+                </Center>
+              );
+            })}
+          </Flex>
+
+          <Center
+            w="32px"
+            h="32px"
+            p="0"
+            size="sm"
+            as={Button}
+            bg="white"
+            variant="outline"
+            isDisabled={currentPage === last(paginationRange)}
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            <ChevronRightIcon boxSize="18px" />
+          </Center>
+        </Flex>
+      </Flex>
+    ),
+    [
+      currentCount,
+      totalCount,
+      pageSize,
+      paginationRange,
+      currentPage,
+      setCurrentPage,
+    ]
+  );
+
+  return {
+    pageSize,
+    setPageSize,
+    currentPage,
+    setCurrentPage,
+    renderPageNumber,
+  };
+};
+
+export default usePagination;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,17 +14,23 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { PeopleResponse } from "interfaces/people.interface";
+import usePagination from "hooks/usePagination";
 
 const Home: NextPage = () => {
   const [data, setData] = React.useState<PeopleResponse | null>(null);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  const { renderPageNumber, currentPage } = usePagination({
+    currentCount: data?.results.length || 0,
+    totalCount: data?.count || 0,
+    siblingCount: 1,
+  });
 
   React.useEffect(() => {
     const getPeople = async () => {
       try {
         setIsLoading(true);
         const resp = await axiosInstance.get("/people");
-        console.log(resp.data, ">>>>data");
         setData(resp.data);
         setIsLoading(false);
       } catch (error) {
@@ -35,59 +41,64 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <Flex p="40px" w="100vw" h="100vh" inset={0}>
-      <TableContainer w="full" bg="white" h="fit-content">
-        <Table variant="simple">
-          {isLoading && (
-            <Tbody>
-              <Tr>
-                <Td colSpan={7} textAlign="center">
-                  <Spinner
-                    color="blue"
-                    thickness="4px"
-                    speed="0.65s"
-                    size="lg"
-                    emptyColor="gray"
-                  />
-                  <Text mt="10px">Loading data</Text>
-                </Td>
-              </Tr>
-            </Tbody>
-          )}
-          <Thead>
-            <Tr>
-              {[
-                "no",
-                "name",
-                "films",
-                "species",
-                "starships",
-                "vehicles",
-                "action",
-              ].map((column, idx) => {
-                return (
-                  <Th fontSize="16px" color="wool-neutral.dark" key={idx}>
-                    {column}
-                  </Th>
-                );
-              })}
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data?.results?.map((person, idx) => (
-              <Tr key={idx}>
-                <Td>{idx}</Td>
-                <Td>{person.name}</Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+    <Flex p="40px" w="100vw" minH="100vh" inset={0} overflowX="hidden">
+      <Flex flexDir="column" w="full" justifyContent="space-between" gap="20px">
+        <Flex p="20px" w="full" bg="white" borderRadius="20px">
+          <TableContainer w="full" h="fit-content">
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  {[
+                    "no",
+                    "name",
+                    "films",
+                    "species",
+                    "starships",
+                    "vehicles",
+                    "action",
+                  ].map((column, idx) => {
+                    return (
+                      <Th fontSize="16px" color="wool-neutral.dark" key={idx}>
+                        {column}
+                      </Th>
+                    );
+                  })}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {isLoading ? (
+                  <Tr>
+                    <Td colSpan={7} textAlign="center" p="50px">
+                      <Spinner
+                        color="blue.500"
+                        thickness="4px"
+                        speed="0.65s"
+                        size="lg"
+                        emptyColor="gray"
+                      />
+                      <Text mt="10px">Loading data</Text>
+                    </Td>
+                  </Tr>
+                ) : (
+                  data?.results?.map((person, idx) => (
+                    <Tr key={idx}>
+                      <Td>{idx}</Td>
+                      <Td>{person.name}</Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td></Td>
+                      <Td></Td>
+                    </Tr>
+                  ))
+                )}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Flex>
+
+        {renderPageNumber}
+      </Flex>
     </Flex>
   );
 };
