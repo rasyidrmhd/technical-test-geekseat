9e6d20a4fcab582280bd91b95482b24b62dded31
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-09-01 04:38:50 +0700

    display data page 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import axiosInstance from "../app/config/axios";
 import {
   Flex,
+  Spinner,
   Table,
   TableContainer,
   Tbody,
@@ -10,21 +11,49 @@ import {
   Th,
   Thead,
   Tr,
+  Text,
 } from "@chakra-ui/react";
+import { PeopleResponse } from "interfaces/people.interface";
 
 const Home: NextPage = () => {
+  const [data, setData] = React.useState<PeopleResponse | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
   React.useEffect(() => {
-    const testApi = async () => {
-      const resp = await axiosInstance.get("/people");
-      console.log(resp.data, ">>>>data");
+    const getPeople = async () => {
+      try {
+        setIsLoading(true);
+        const resp = await axiosInstance.get("/people");
+        console.log(resp.data, ">>>>data");
+        setData(resp.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+      }
     };
-    testApi();
+    getPeople();
   }, []);
 
   return (
-    <Flex p="40px">
-      <TableContainer w="full">
-        <Table variant="striped">
+    <Flex p="40px" w="100vw" h="100vh" inset={0}>
+      <TableContainer w="full" bg="white" h="fit-content">
+        <Table variant="simple">
+          {isLoading && (
+            <Tbody>
+              <Tr>
+                <Td colSpan={7} textAlign="center">
+                  <Spinner
+                    color="blue"
+                    thickness="4px"
+                    speed="0.65s"
+                    size="lg"
+                    emptyColor="gray"
+                  />
+                  <Text mt="10px">Loading data</Text>
+                </Td>
+              </Tr>
+            </Tbody>
+          )}
           <Thead>
             <Tr>
               {[
@@ -44,7 +73,19 @@ const Home: NextPage = () => {
               })}
             </Tr>
           </Thead>
-          <Tbody></Tbody>
+          <Tbody>
+            {data?.results?.map((person, idx) => (
+              <Tr key={idx}>
+                <Td>{idx}</Td>
+                <Td>{person.name}</Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
       </TableContainer>
     </Flex>
